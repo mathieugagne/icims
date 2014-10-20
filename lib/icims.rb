bc064921cd0f93186557fbc4dcc95e21ec87ce9e
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-10-20 09:34:50 -0400

    Remove Sync & JobParser since not generic enough

--- lib/icims.rb
+++ lib/icims.rb
@@ -2,8 +2,6 @@ require 'icims/requestable'
 require 'icims/get_request'
 require 'icims/search_request'
 require 'icims/job'
-require 'icims/job_parser'
-require 'icims/sync'
 
 module ICIMS
   require 'net/https'
