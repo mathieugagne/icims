29603acebf68c2b18c080805d63c9076987cbc4b
Mathieu Gagne <mgagne@vigilantglobal.com>
2016-02-01 13:59:42 -0500

    Adds validations on ICIMS.customer_id

--- lib/icims/requestable.rb
+++ lib/icims/requestable.rb
@@ -1,12 +1,11 @@
 module ICIMS
   module Requestable
 
-    BASE_URL = "https://api.icims.com/customers/"
-
     private
 
     def customer_url
-      BASE_URL + ICIMS.customer_id
+      raise ArgumentError, "You must provide a `customer_id`. e.g. ICIMS.customer_id = 1234" unless ICIMS.customer_id.present?
+      "https://api.icims.com/customers/#{ICIMS.customer_id}"
     end
 
   end
